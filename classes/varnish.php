df2d3d3d6b6c35d690644fa6502d0e977722a032
Alex Pilyvskiy <spi@nxc.no>
2013-09-04 16:01:44 +0300

    add single quotes for complex requests

--- classes/varnish.php
+++ classes/varnish.php
@@ -37,7 +37,7 @@ class nxcVarnish
 
 	public function cli( $request, $background = false ) {
 		$request = escapeshellcmd( trim( $request ) );
- 		$cmd     = 'varnishadm -T ' . $this->host . ':' . $this->port . ' -t ' . $this->port . ' ' . $request;
+ 		$cmd     = 'varnishadm -T ' . $this->host . ':' . $this->port . ' -t ' . $this->port . " ' " . $request . " ' ";
  		if( $background ) {
  			 exec( $cmd . ' > /dev/null 2>&1 &' );
  			 return true;
